bb8d909c624a55faf6d4e10283dd021ea03d46c1
Gustavo Braun <gtvbraun@gmail.com>
2022-07-18 20:22:14 -0300

    agregar boton

--- src/componentes/boton.js
+++ src/componentes/boton.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import '../hojas-de-estilo/Boton.css';
 
 function Boton({ texto, esBotonDeClic, manejarClic }) {
 	return(
 		<button 
-			className={esBotonDeClic ? 'boton-clic' : 'boton-reiniciar'}
-			onClick={}>
+			className={ esBotonDeClic ? 'boton-clic' : 'boton-reiniciar' }
+			onClick={manejarClic}>
 			{texto}
 		</button>
 	);
 }
+
+export default Boton;
