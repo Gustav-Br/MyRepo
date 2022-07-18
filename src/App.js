d94e50d50de0b68ee3effe4a71dd5a083cf78c3a
Gustavo Braun <gtvbraun@gmail.com>
2022-07-18 20:14:39 -0300

    agregando

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import './App.css';
+import Boton from './componentes/boton.js'
+import Contador from './componentes/Contador.js'
 import freeCodeCampLogo from './imagenes/logofcc.png';
 
 function App() {
+
+  const manejarClic = () => {
+    console.log('ClicMe');
+  }
+
+  const reiniciarContador = () => {
+    console.log('Reiniciar');
+  }
+
   return (
     <div className='App'>
       <div className='logo-contenedor'>
@@ -10,8 +21,16 @@ function App() {
           src={freeCodeCampLogo}
           alt='Logo de fcc'/> 
       </div>
-      <div>
-
+      <div className='contenedor-ppal'>
+        <Contador numClics='5' />
+        <Boton 
+          texto='Clic'
+          esBotonDeClic={true}
+          manejarClic={manejarClic} />
+        <Boton 
+          texto='Reiniciar'
+          esBotonDeClic={false}
+          manejarClic={reiniciarContador} />
       </div>  
     </div>
   );
